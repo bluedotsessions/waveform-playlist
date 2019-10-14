1690dd3479b44c35b5a17cc04ef00301792b0bec
Anton Stoyanov <tzat.anton@gmail.com>
2019-10-14 17:29:30 +0300

    load the example stem file

--- controller.js
+++ controller.js
@@ -1,59 +1,83 @@
 var playlist = WaveformPlaylist.init({
   samplesPerPixel: 3000,
-  mono: false,
   waveHeight: 100,
   container: document.getElementById("playlist"),
-  state: 'cursor',
+  state: 'interactive',
+  bpm:90,
+  quantize:1,
   colors: {
-      waveOutlineColor: '#E0EFF1',
-      timeColor: 'grey',
-      fadeColor: 'black'
+    waveOutlineColor: '#E0EFF1',
+    timeColor: 'grey',
+    fadeColor: 'black'
   },
   timescale: true,
   controls: {
     show: true, //whether or not to include the track controls
     width: 200 //width of controls in pixels
   },
-  seekStyle : 'fill',
+  seekStyle : 'line',
   zoomLevels: [500, 1000, 3000, 5000]
 });
-
 playlist.load([
+
   {
-    "src": "media/audio/Vocals30.mp3",
-    "name": "Vocals",
-    "fadeIn": {
-      "duration": 0.5
-    },
-    "fadeOut": {
-      "duration": 0.5
-    },
-    "cuein": 5.918,
-    "cueout": 14.5
+    "src": "audio/01_Um Pepino_Electric Guitar.mp3 ",
+    "name": "01",
+    "cuein": 0,
+    "waveOutlineColor": '#c0dce0'
   },
   {
-    "src": "media/audio/BassDrums30.mp3",
-    "name": "Drums",
-    "start": 8.5,
-    "fadeIn": {
-      "shape": "logarithmic",
-      "duration": 0.5
-    },
-    "fadeOut": {
-      "shape": "logarithmic",
-      "duration": 0.5
-    }
+    "src": "audio/02_Um Pepino_Vibraphone.mp3 ",
+    "name": "02",
+    "cuein": 0,
+    "waveOutlineColor": '#c0dce0'
   },
   {
-    "src": "media/audio/Guitar30.mp3",
-    "name": "Guitar",
-    "start": 23.5,
-    "fadeOut": {
-      "shape": "linear",
-      "duration": 0.5
-    },
-    "cuein": 15
-  }
+    "src": "audio/03_Um Pepino_Volda Drumsyn.mp3 ",
+    "name": "03",
+    "cuein": 0,
+    "waveOutlineColor": '#c0dce0'
+  },
+  {
+    "src": "audio/04_Um Pepino_Volda Drumsyn.mp3 ",
+    "name": "04",
+    "cuein": 0,
+    "waveOutlineColor": '#c0dce0'
+  },
+  {
+    "src": "audio/05_Um Pepino_Drum Kit.mp3 ",
+    "name": "05",
+    "cuein": 0,
+    "waveOutlineColor": '#c0dce0'
+  },
+  {
+    "src": "audio/06_Um Pepino_Rhodia Synpad.mp3 ",
+    "name": "06",
+    "cuein": 0,
+    "waveOutlineColor": '#c0dce0'
+  },
+  {
+    "src": "audio/07_Um Pepino_Fjoria Syn.mp3 ",
+    "name": "07",
+    "cuein": 0,
+    "waveOutlineColor": '#c0dce0'
+  },
+  {
+    "src": "audio/08_Um Pepino_SFX.mp3 ",
+    "name": "08",
+    "cuein": 0,
+    "waveOutlineColor": '#c0dce0'
+  },
+  {
+    "src": "audio/09_Um Pepino_SFX 2.mp3",
+    "name": "09",
+    "cuein": 0,
+    "waveOutlineColor": '#c0dce0'
+  },
+
 ]).then(function() {
   //can do stuff with the playlist.
-});
+
+  //initialize the WAV exporter.
+  playlist.initExporter();
+}); 
